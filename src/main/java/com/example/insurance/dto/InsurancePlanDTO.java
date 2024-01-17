e18a2e0ce6d0337bb8ad4f5bba8b2c8dd60f2494
20120583 <trananhthi56@gmail.com>
2024-01-17 21:47:52 +0700

    'fix'

--- src/main/java/com/example/insurance/dto/InsurancePlanDTO.java
+++ src/main/java/com/example/insurance/dto/InsurancePlanDTO.java
@@ -2,7 +2,6 @@ package com.example.insurance.dto;
 
 import com.example.insurance.entity.InsurancePlanPrice;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,8 +9,7 @@ import java.util.List;
 
 @Getter
 @Setter
-@AllArgsConstructor
-@JsonIgnoreProperties(value = {"forms","createdAt","updatedAt","status"})
+@JsonIgnoreProperties(value = {"createdAt","updatedAt","status"})
 public class InsurancePlanDTO {
     private Long id;
     private String planName;
@@ -19,11 +17,17 @@ public class InsurancePlanDTO {
     private int accidentInsurance;
     private int hospitalization;
     private int surgery;
+
     private int beforeAdmission;
+
     private int afterDischarge;
+
     private int takeCareAtHome;
+
     private int hospitalizationAllowance;
+
     private int emergencyTransport;
+
     private int funeralAllowance;
     private List<InsurancePlanPrice> prices ;
     private  List<InsuranceBenefitDTO> benefits;
